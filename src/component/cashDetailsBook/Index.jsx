fad0333cd91c13df656ec77cd62a92b51d13b9af
Mwaqas786 <onidea789@gmail.com>
2024-01-20 19:12:01 +0500

    add new page

---
+++ src/component/cashDetailsBook/Index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Index = () => {
+  return (
+    <div>Cash Details Book</div>
+  )
+}
+
+export default Index
---
+++ src/component/expence/Index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Index = () => {
+  return (
+    <div>Expence</div>
+  )
+}
+
+export default Index
